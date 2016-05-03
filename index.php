a776aaea64408c620a80cf7320fa5b28fdcc0a95
Ankur Biswas <iankurbiswas@outlook.com>
2016-05-03 17:02:02 +0530

    Added project files

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sign In</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<!-- JavaScript -->
+	<script src="js/respond.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row col-lg-12 col-md-12 col-sm-12">
+			<img src="images/logo.png" class="img-responsive tocenter" alt="KITM Logo">
+			<p class="sign text-center"> Sign In </p>
+			<p class="text-center info"> Use your KITM Account.</p>
+			<p class="text-center what"> <a href=""> What's This? </a> </p>
+			<div class="col-md-3 col-sm-2 col-xs-1"></div>
+			<div class="col-md-6 col-sm-8 col-xs-10 ">
+				<form action="" method="POST">
+					<input type="text" class="form-control name" placeholder="Username">
+					<input type="password" class="form-control name" placeholder="Password">
+					<div class="checkbox">
+	  					<label class="check"><input type="checkbox" value="">Keep me signed in</label>
+					</div>
+					<input type="submit" value="Sign in" class="form-control name submit">
+				</form>
+				<p class="text-center account"> No account? Contact Admin!</p>
+				<p class="text-center forget"> <a href=""> Forgotten my password </a> </p>
+				<p class="text-center company"> Kurukshetra Institute of Technology & Management </p>
+			</div>
+			<div class="col-md-3 col-sm-2 col-xs-1"></div>
+		</div> <!-- row -->
+	</div> <!-- container -->
+	<!-- javaScript -->
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
